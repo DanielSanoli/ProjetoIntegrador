559978cd3de91ae8ef500e05c5eb0468526c20b2
yuri <yuri.mtg@hotmail.com>
2021-03-30 13:32:41 -0300

    Commit Lab 8(Yuri)

--- Mercado/src/br/com/application/Main.java
+++ Mercado/src/br/com/application/Main.java
@@ -3,14 +3,6 @@ package br.com.application;
 public class Main {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
-        System.out.println("commit teste");
-        
-        System.out.println("Teste");
-        System.out.println ("Ola");
-        
-        System.out.println ("Teste Lucats");
                 
     }
 
