aabfc34077a2216ef3e69587cd557072560f8014
mathccastro <matheusacmcastro@gmail.com>
2021-03-30 22:11:00 -0300

    Finalizado implementação das telas de venda e meio de pagamento.

---
+++ Mercado/src/br/com/application/utils/Utils.java
@@ -0,0 +1,11 @@
+package br.com.application.utils;
+
+import javax.swing.JFrame;
+
+public class Utils {
+
+    public static void configuracaoInicial(JFrame frame) {
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+    }
+}
