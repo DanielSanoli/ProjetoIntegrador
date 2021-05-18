73b344daec9d9a8a915a022cf572ef1e33f6b5b7
Matheus Castro <mcastro@sweda.com.br>
2021-05-18 15:55:59 -0300

    s

--- Mercado/src/br/com/application/Main.java
+++ Mercado/src/br/com/application/Main.java
@@ -10,21 +10,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
-        DecimalFormat df = new DecimalFormat("###,##0.00");
-
-        // Teste de venda
-        int numeroVenda = VendaController.cadastrar(new Date(System.currentTimeMillis()), 1, 1, Double.parseDouble(df.format(random.nextDouble() * 100).replace(",", ".")));
-
-        for (int i = 1; i <= 10; i++) {
-
-            System.out.println("Iniciando teste número #" + i);
-
-            // Teste de itemVenda
-            boolean res = ItemVendaController.cadastrar(numeroVenda, 1, 20);
-
-            System.out.println("Finalizando teste... Resultado: " + (res ? "Sucesso" : "Falha"));
-
-        }
+//        Random random = new Random();
+//        DecimalFormat df = new DecimalFormat("###,##0.00");
+//
+//        // Teste de venda
+//        int numeroVenda = VendaController.cadastrar(new Date(System.currentTimeMillis()), 1, 1, Double.parseDouble(df.format(random.nextDouble() * 100).replace(",", ".")));
+//
+//        for (int i = 1; i <= 10; i++) {
+//
+//            System.out.println("Iniciando teste número #" + i);
+//
+//            // Teste de itemVenda
+//            boolean res = ItemVendaController.cadastrar(numeroVenda, 1, 20);
+//
+//            System.out.println("Finalizando teste... Resultado: " + (res ? "Sucesso" : "Falha"));
+//
+//        }
     }
 }
