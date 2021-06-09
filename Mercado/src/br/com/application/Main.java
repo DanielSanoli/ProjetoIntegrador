66791450d26aad1a83159e5dda17bb7e66e09271
daniDaniel <daniel.sanoli28@gmail.com>
2021-06-09 10:36:31 -0300

    Projeto Integrador completo.

--- Mercado/src/br/com/application/Main.java
+++ Mercado/src/br/com/application/Main.java
@@ -5,7 +5,7 @@ import br.com.application.view.LoginView;
 public class Main {
 
     public static void main(String[] args) {
-
+        // Abrir programa
         LoginView lv = new LoginView();
         lv.setVisible(true);
         
